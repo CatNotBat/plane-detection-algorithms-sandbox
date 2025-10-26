439d215b50c09f5c0852b19c78af7ab39f230213
CatNotBat <thatsoup1@gmail.com>
2025-10-26 13:40:14 +0200

    moved ransac+svd from main to function

--- main.py
+++ main.py
@@ -3,22 +3,29 @@ import numpy as np
 from ransac import ransac
 from svd import svd
 
-if __name__ == "__main__":
-    ITERATIONS = 5000
-    DISTANCE_THRESHOLD = 0.05
-    start_time = time.time()
 
-    points = np.loadtxt("my_test_plane.xyz")
-    filtered_points = ransac(points, ITERATIONS, DISTANCE_THRESHOLD)[0]
+def ransac_svd(points: np.ndarray, iterations: int = 2000, distance_threshold: float = 0.05):
+    filtered_points = ransac(points, iterations, distance_threshold)[0]
     normal = svd(filtered_points)[2][-1, :]
 
     if normal[2] < 0:
         normal = -normal
 
-    A = normal[0]
-    B = normal[1]
-    C = normal[2]
+    a = normal[0]
+    b = normal[1]
+    c = normal[2]
     centroid = np.mean(filtered_points, axis=0)
-    D = -centroid @ normal
+    d = -centroid @ normal
 
-    print(f"Best Plane: {A}x + {B}y + {C}z + {D} time: {time.time() - start_time:.2f} seconds")
+    return a, b, c, d
+
+
+if __name__ == "__main__":
+    ITERATIONS = 5000
+    DISTANCE_THRESHOLD = 0.05
+    start_time = time.time()
+
+    data_points = np.loadtxt("my_test_plane.xyz")
+    plane = ransac_svd(data_points, ITERATIONS, DISTANCE_THRESHOLD)
+
+    print(f"Best Plane: {plane[0]}x + {plane[1]}y + {plane[2]}z + {plane[3]} time: {time.time() - start_time:.2f} seconds")
